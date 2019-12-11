7a6469384740a97a0eb11138fa78467f287f039d
Alwin Thomas <alwinthomas@MacBook-Pro.local>
2019-12-11 14:45:25 +0000

    initial commit

---
+++ src/test/java/com/project/booksoft/BookSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.booksoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
